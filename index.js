71ddac5fb88f1300fdb45a1d95a4cc132064b4fd
Shayma MERZOUGUI <shayma.merzougui@ecole-isitech.fr>
2023-04-08 17:53:02 +0200

    save

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mysql from 'mysql2';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
 
 
 
 
 // creation de la connection mysql
+
+
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -21,6 +25,19 @@ const connection = mysql.createConnection({
 //initialisation de l'application Express
 const app = express();
 
+app.use(cookieParser());
+app.use(session({ secret: "Shh, its a secret!" }));
+
+app.get('/', function (httpRequest, httpResponse) {
+    if (httpRequest.session.page_views) {
+        httpRequest.session.page_views++;
+        httpResponse.send("You visited this page " + httpRequest.session.page_views + " times");
+        console.log(httpRequest.session)
+    } else {
+        httpRequest.session.page_views = 1;
+        httpResponse.send("Welcome to this page for the first time!");
+    }
+});
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
@@ -83,35 +100,41 @@ app.get('/nouscontacter', function (httpRequest, httpResponse, next) {
 });
 
 app.get('/panier', function (httpRequest, httpResponse, next) {
-    //log de la requete entrante
-    console.log('objet request: ', httpRequest);
-    //envoie de la reponse http
-    httpResponse.render('pages/panier')
-});
-
-
-
-
-app.get('/nylasN16', function (httpRequest, httpResponse, next) {
-    //log de la requete entrante
-    connection.query('select * from products', (err, result, fields) => {
-        console.log(result);
-        //envoie de la reponse h)ttp
-        httpResponse.render('pages/nylasN16', { products: result })
+    var produits = []
+    var price = 0
+    connection.query("SELECT * FROM commande WHERE id_client = 0 ", async (err, rows) => {
+        for (const element of rows) {
+            var rawElement = await getElement(element)
+            price += rawElement.price
+            produits.push(rawElement)
+        }
+        console.log(produits)
+        httpResponse.render('pages/panier', { produits: produits, price: price })
     })
 });
 
-app.post('/panier', function (httpRequest, httpResponse, next) {
-    console.log(httpRequest.body)
+function getElement(elem) {
+    return new Promise((resolve, reject) => {
+        connection.query("SELECT * FROM produits WHERE id = ? ", [elem.id_produit], (err, rows) => {
+            if (err) throw err;
+            resolve(rows[0])
+        })
+    })
+}
+
+
+
+
+
+app.post('/ajoutpanier', function (httpRequest, httpResponse, next) {
+    console.log(httpRequest.body.product_id)
+    connection.query('insert into commande (id_client, id_produit) values (?)', [[0, parseInt(httpRequest.body.product_id)]],
+        function (err, result) {
+            if (err) throw err;
+        }
+    )
 })
 
-
-app.get('/panier', function (httpRequest, httpResponse, next) {
-    //log de la requete entrante
-        //envoie de la reponse h)ttp
-        httpResponse.render('pages/panier')
-        console.log(httpRequest)
-    });
 
 
 
